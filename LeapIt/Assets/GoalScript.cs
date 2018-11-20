8f44b0fb32231ebb8217150d56db4c86be9c1fc7
Alex Beach <ajbeach67@gmail.com>
2018-11-20 15:41:26 -0500

    prototype

---
+++ LeapIt/Assets/GoalScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            if (PlayerScript.moves <= 1)
+                Change();
+        }
+
+
+    }
+    public void Change()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", Color.green);
+    }
+}
---
+++ LeapIt/Assets/PlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public int startMoves;
+    public static int moves; 
+
+    // Use this for initialization
+    void Start () {
+        moves = startMoves;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(new Vector3(0, 0, 1));
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(new Vector3(0, 0, -1));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(new Vector3(1, 0, 0));
+        }
+    }
+
+    void Move(Vector3 v)
+    {
+        if (moves > 0)
+        {
+            transform.position += v;
+            moves--;
+        }
+      
+        
+    }
+}
